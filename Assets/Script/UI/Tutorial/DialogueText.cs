cc0750e30c2266fc180fb31e64ac94974aff2121
cwhk-4 <57931058+cwwww424@users.noreply.github.com>
2020-12-01 19:26:14 +0900

    1201 Tutorial

---
+++ Assets/Script/UI/Tutorial/DialogueText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueText : MonoBehaviour
+{
+    public static string getDialogue( int dialogueNum )
+    {
+        string str = null;
+
+        switch( dialogueNum )
+        {
+            case 1:
+            str = "動物箱庭にようこそ!\n\n動物を箱庭に招き入れて自分だけの箱庭を完成させましょう!";
+            break;
+            case 2:
+            str = "動物はエサを求めて箱庭にやってきます\n\nまずはアイテムから草を配置しましょう！";
+            break;
+            case 3:
+            str = "草を選択できたので、実際に箱庭に配置してみましょう!";
+            break;
+            case 4:
+            str = "草が配置できました!\n\nあとは動物が来るのを待ちましょう!";
+            break;
+            case 5:
+            str = "これでシマウマが箱庭に配置されました!\n\n一旦、様子を見てみましょう...。";
+            break;
+            case 6:
+            str = "草が枯れてしまい、ライオンも来てしまいました...。\n\nこのままでは危ないので、枯れ草を\n\nどうぐで修繕しましょう!";
+            break;
+            case 7:
+            str = "どうぐになりましたね！\n\nそれでは枯れた草を左クリック長押しで修繕しましょう";
+            break;
+            case 8:
+            str = "草を修繕したことで、再びシマウマが集まりました！\n\nライオンは狩りの成功率が下がったため、\n\n一旦引いていきます。";
+            break;
+            case 9:
+            str = "かんきょうを置いて、新しい動物を発見して\nはってんさせ、たくさんの動物を発見しましょう！";
+            break;
+
+            default:
+            str = null;
+            break;
+        }
+
+        return str;
+    }
+
+}
---
+++ Assets/Script/UI/Tutorial/DisplayDialogue.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DisplayDialogue : MonoBehaviour
+{
+    public TMP_Text dialogue;
+    
+    private int dialogueCount = 1;
+
+    public GameObject dialogueWindow;
+
+    private void Start() 
+    {
+        dialogueWindow = this.transform.parent.gameObject;
+        dialogue = this.GetComponent<TMP_Text>();
+        dialogue.text = DialogueText.getDialogue(dialogueCount);
+    }
+
+    void Update( )
+    {
+        if( Input.GetMouseButtonDown( 0 ) )
+        {
+            if( dialogueCount < 9 )
+            {
+                nextDialogue();
+            }
+            dialogue.text = DialogueText.getDialogue(dialogueCount);
+        }
+    }
+
+    private void nextDialogue()
+    {
+        switch(dialogueCount)
+        {
+            case 1:
+            case 7:
+            case 8:
+                dialogueCount++;
+                break;
+
+            case 2:
+                //click grass
+                dialogueCount++;
+                break;
+
+            case 3:
+                //finish grass
+                dialogueCount++;
+                break;
+
+            case 4:
+                //zebra come
+                dialogueCount++;
+                break;
+
+            case 5:
+                //lion come
+                dialogueCount++;
+                break;
+
+            case 6:
+                //fix grass
+                dialogueCount++;
+                break;
+        }
+    }
+
+    private void switchInvisibility(bool on)
+    {
+        dialogueWindow.GetComponent<RawImage>().enabled = on;
+        dialogue.enabled = on;
+        //caution
+        //instruction enable = on;
+    }
+}
