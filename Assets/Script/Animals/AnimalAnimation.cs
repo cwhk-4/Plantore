a658df3aa0de360ce13af6dc34e4562b6572b70a
cwhk-4 <57931058+cwwww424@users.noreply.github.com>
2020-12-05 07:55:36 +0900

    1205 zebra animation

---
+++ Assets/Script/Animals/AnimalAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAnimation : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    public Sprite[] sprites;
+
+    private int i = 0;
+    private float count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[i];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+
+        if( count % 6 == 0)
+        {
+            spriteRenderer.sprite = sprites[i];
+            i++;
+            if( i >= sprites.Length - 1 )
+            {
+                i = 0;
+            }
+        }
+    }
+}
