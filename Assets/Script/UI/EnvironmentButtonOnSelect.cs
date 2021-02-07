722941e306b3d6d698753833999bb304016c20e3
cwhk-4 <57931058+cwwww424@users.noreply.github.com>
2021-02-08 00:26:50 +0900

    update environment

---
+++ Assets/Script/UI/EnvironmentButtonOnSelect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EnvironmentButtonOnSelect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private RawImage ParentImage;
+
+    [SerializeField] private Texture OnSelectImage;
+    [SerializeField] private Texture NormalImage;
+
+    void Start()
+    {
+        ParentImage = GetComponentInParent<RawImage>( );
+    }
+
+    public void OnPointerExit( PointerEventData eventData )
+    {
+        ParentImage.texture = NormalImage;
+    }
+
+    public void OnPointerEnter( PointerEventData eventData )
+    {
+        ParentImage.texture = OnSelectImage;
+    }
+
+}
