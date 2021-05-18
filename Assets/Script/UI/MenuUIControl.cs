22713b17def45354687f523afbbad66d203d85aa
cwhk-4 <57931058+cwwww424@users.noreply.github.com>
2021-05-19 00:27:25 +0900

    #19 未完成
    入れ替えはしたが
    PLとGGに相談が必要

--- Assets/Script/UI/MenuUIControl.cs
+++ Assets/Script/UI/MenuUIControl.cs
@@ -6,35 +6,9 @@ public class MenuUIControl : MonoBehaviour
     [SerializeField] private GameObject Menu;
     [SerializeField] Image MenuButton;
 
-    [SerializeField] private Sprite Open;
-    [SerializeField] private Sprite Close;
-
     private void Start( )
     {
         Menu.SetActive( false );
-        CloseMenu( );
-    }
-
-    public void MenuButtonClick( )
-    {
-        if( Menu.activeSelf )
-        {
-            CloseMenu( );
-        }
-        else
-        {
-            OpenMenu( );
-        }
-    }
-
-    public void CloseMenu( )
-    {
-        MenuButton.sprite = Close;
-    }
-
-    public void OpenMenu( )
-    {
-        MenuButton.sprite = Open;
     }
 
     public void InitUI( )
