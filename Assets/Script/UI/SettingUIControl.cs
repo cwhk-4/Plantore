20e15b38a7363bd02794c2ce31db920011dd5a5a
cwhk-4 <57931058+cwwww424@users.noreply.github.com>
2021-05-29 10:09:39 +0900

    #21

---
+++ Assets/Script/UI/SettingUIControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SettingUIControl : MonoBehaviour
+{
+    [SerializeField] private GameObject settingUI;
+
+    void Start()
+    {
+        CloseSettingUI( );
+    }
+
+    public void CloseSettingUI( )
+    {
+        settingUI.SetActive( false );
+    }
+
+    public void OpenSettingUI( )
+    {
+        settingUI.SetActive( true );
+    }
+}
