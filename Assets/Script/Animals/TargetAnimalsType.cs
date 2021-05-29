c24e4704ba4f4038b47547cd779f25a8c11ed7bf
cwhk-4 <57931058+cwwww424@users.noreply.github.com>
2021-05-29 19:29:39 +0900

    minor

--- Assets/Script/Animals/TargetAnimalsType.cs
+++ Assets/Script/Animals/TargetAnimalsType.cs
@@ -1,34 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TargetAnimalsType : MonoBehaviour
 {
     public static bool canPredation;
     public static GameObject targetAnimal;
+    //[SerializeField] private bool haveTargetAnimal;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //haveTargetAnimal = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( LION._lion.canMove && ( ZEBRA._zebra.canMove || GIRAFFE._giraffe.canMove ) )
+        if ( LION._lion.canMove && ( ZEBRA._zebra.canMove || GIRAFFE._giraffe.canMove ) /*&& !haveTargetAnimal*/)
         {
             canPredation = true;
-            if ( GIRAFFE._giraffe.canMove == false )
+            if( GIRAFFE._giraffe.canMove == false )
             {
                 targetAnimal = ZEBRA._zebra.animals;
-            } else if( ZEBRA._zebra.canMove == false )
+            }
+            else if( ZEBRA._zebra.canMove == false )
             {
                 targetAnimal = GIRAFFE._giraffe.animals;
-            } else
+            }
+            else if( ZEBRA._zebra.canMove && GIRAFFE._giraffe.canMove )
             {
                 randomTargetAnimal( );
             }
-        }else
+            else
+            {
+                Debug.Log( "noA" );
+            }
+
+            Debug.Log( "noB" );
+            //haveTargetAnimal = true;
+            
+        }
+        else
         {
             canPredation = false;
             targetAnimal = null;
