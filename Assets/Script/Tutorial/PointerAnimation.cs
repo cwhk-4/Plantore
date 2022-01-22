e54a7350a5d4891e7760497c02f7db22922ad0e3
cwhk-4 <57931058+cwhk-4@users.noreply.github.com>
2022-01-22 17:35:15 +0900

    tutorial guide speed

--- Assets/Script/Tutorial/PointerAnimation.cs
+++ Assets/Script/Tutorial/PointerAnimation.cs
@@ -5,14 +5,23 @@ public class PointerAnimation : MonoBehaviour
     [SerializeField] private RectTransform RectTransform;
     [SerializeField] private Vector2 OriginalVec;
     [SerializeField] private Vector2 TargetVec;
-    [SerializeField] private int x = 0;
-    [SerializeField] private int y = 0;
-    [SerializeField] private int dir = 1;
+    [SerializeField] private float x = 0;
+    [SerializeField] private float y = 0;
+    [SerializeField] private float dir = 0.75f;
     [SerializeField] private int distance = 15;
 
     void Start()
     {
         OriginalVec = RectTransform.anchoredPosition;
+    }
+
+    void Init( )
+    {
+        x = y = 0;
+        if( dir < 0 )
+        {
+            dir *= -1;
+        }
     }
 
     public void ChangePos( Vector2 newPos )
@@ -25,8 +34,7 @@ public class PointerAnimation : MonoBehaviour
 
         RectTransform.anchoredPosition = newPos;
         OriginalVec = newPos;
-        x = y = 0;
-        dir = 1;
+        Init( );
     }
 
     private void Update( )
